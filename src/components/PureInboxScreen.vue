a785c11e74eca09e138ddeaed8798391c87605a6
dan9b6 <dkburton0@gmail.com>
2024-02-27 14:27:43 +0000

    Tweak to ui for chromatic

--- src/components/PureInboxScreen.vue
+++ src/components/PureInboxScreen.vue
@@ -2,9 +2,9 @@
   <div>
     <div v-if="error" class="page lists-show">
       <div class="wrapper-message">
-        <span class="icon-face-sad" />
         <p class="title-message">Oh no!</p>
         <p class="subtitle-message">Something went wrong</p>
+        <span class="icon-face-sad" />
       </div>
     </div>
     <div v-else class="page lists-show">
